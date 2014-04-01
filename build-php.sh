8a41b98a8854ca881beb858c3461c0b73203bba4
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-04-01 16:15:53 -0500

    Updating build script

--- build-php.sh
+++ build-php.sh
@@ -18,10 +18,10 @@ tar -xf php-$VERSION.tar.gz
 cd /tmp/php-$VERSION
 cp $SCRIPTPATH/*-pak .
 
-./configure  '--with-libdir=lib64' '--with-config-file-path=/etc/php' '--with-config-file-scan-dir=/etc/php/conf.d' '--with-pic' '--without-gdbm' '--with-gettext' '--with-iconv' '--with-openssl' '--with-zlib' '--with-layout=GNU' '--enable-exif' '--enable-ftp' '--enable-sockets' '--enable-sysvsem' '--enable-sysvshm' '--enable-sysvmsg' '--with-kerberos' '--enable-shmop' '--enable-calendar' '--with-mysql' '--with-gd' '--with-mcrypt' '--enable-pdo' '--with-pdo-mysql=mysqlnd' '--enable-json' '--with-curl' '--enable-fpm' '--enable-mbstring' '--enable-inline-optimization' '--enable-pcntl' '--enable-mbregex' '--with-mhash' '--with-pcre-regex' '--with-fpm-user=www-data' '--enable-zip' '--with-mysqli=mysqlnd' '--with-sqlite3' '--with-readline' '--enable-opcache' '--enable-soap'
+./configure  '--with-libdir=lib64' '--with-config-file-path=/etc/php' '--with-config-file-scan-dir=/etc/php/conf.d' '--with-pic' '--without-gdbm' '--with-gettext' '--with-iconv' '--with-openssl' '--with-zlib' '--with-layout=GNU' '--enable-exif' '--enable-ftp' '--enable-sockets' '--enable-sysvsem' '--enable-sysvshm' '--enable-sysvmsg' '--with-kerberos' '--enable-shmop' '--enable-calendar' '--with-mysql' '--with-gd' '--with-mcrypt' '--enable-pdo' '--with-pdo-mysql=mysqlnd' '--enable-json' '--with-curl' '--enable-fpm' '--enable-mbstring' '--enable-inline-optimization' '--enable-pcntl' '--enable-mbregex' '--with-mhash' '--with-pcre-regex' '--with-fpm-user=www-data' '--enable-zip' '--with-mysqli=mysqlnd' '--with-sqlite3' '--with-readline' '--enable-opcache' '--enable-soap' '--with-jpeg-dir' '--with-png-dir'
 
 # Install the init script
-cp /tmp/php-$VERSION/sapi/fpm/init.d.php-fpm.in /etc/init.d/php-fpm
+cp /tmp/php-$VERSION/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
 update-rc.d /etc/init.d/php-fpm defaults
 
 mkdir -p /etc/php/conf.d

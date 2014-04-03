0bcf1d3da02213e7d4839826c0c8836149229be7
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-04-03 12:14:51 -0500

    Updating build materials

--- build-php.sh
+++ build-php.sh
@@ -14,6 +14,13 @@ cd /tmp
 wget http://us1.php.net/get/php-$VERSION.tar.gz/from/this/mirror -O php-$VERSION.tar.gz
 tar -xf php-$VERSION.tar.gz
 
+
+cd $SCRIPTPATH
+echo "Copying init.d and checkinstall file"
+cp init-php-fpm /etc/init.d/php-fpm
+cp init-php-fpm /tmp/php-$VERSION/init-php-fpm
+cp setup /tmp/php-$VERSION/setup
+
 # Copy the Script Paths
 cd /tmp/php-$VERSION
 cp $SCRIPTPATH/*-pak .
@@ -21,10 +28,8 @@ cp $SCRIPTPATH/*-pak .
 ./configure  '--with-libdir=lib64' '--with-config-file-path=/etc/php' '--with-config-file-scan-dir=/etc/php/conf.d' '--with-pic' '--without-gdbm' '--with-gettext' '--with-iconv' '--with-openssl' '--with-zlib' '--with-layout=GNU' '--enable-exif' '--enable-ftp' '--enable-sockets' '--enable-sysvsem' '--enable-sysvshm' '--enable-sysvmsg' '--with-kerberos' '--enable-shmop' '--enable-calendar' '--with-mysql' '--with-gd' '--with-mcrypt' '--enable-pdo' '--with-pdo-mysql=mysqlnd' '--enable-json' '--with-curl' '--enable-fpm' '--enable-mbstring' '--enable-inline-optimization' '--enable-pcntl' '--enable-mbregex' '--with-mhash' '--with-pcre-regex' '--with-fpm-user=www-data' '--enable-zip' '--with-mysqli=mysqlnd' '--with-sqlite3' '--with-readline' '--enable-opcache' '--enable-soap' '--with-jpeg-dir' '--with-png-dir'
 
 # Install the init script
-cp /tmp/php-$VERSION/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
-update-rc.d /etc/init.d/php-fpm defaults
+$(which update-rc.d) /etc/init.d/php-fpm defaults
 
 mkdir -p /etc/php/conf.d
-cp setup /tmp/php-$VERSION/setup
 make -j2
 make install

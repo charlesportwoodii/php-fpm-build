51ef5e74880ca06dc0d491777cd17e1d6bc14d7b
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-04-28 20:45:53 +0000

    Updating PHP for cross lsb_release builds

--- build-php.sh
+++ build-php.sh
@@ -35,6 +35,6 @@ make -j2
 make install
 
 cd /tmp/php-$VERSION
-sudo checkinstall -D -pkgname php-fpm  -pkglicense PHP -pkggroup PHP -maintainer charlesportwoodii@ethreal.net -provides "php-fpm, php-fpm-5.6"  -requires "libxml2, libxml2-dev, libcurl4-openssl-dev, libmcrypt-dev, libmcrypt4, libjpeg-turbo8, libjpeg-turbo8-dev, libpng12-dev, libltdl-dev, libreadline-dev" -replaces "php-fpm-5.5" -conflicts "php, php-common" -pakdir /tmp/ -y sh /tmp/php-$VERSION/setup
+sudo checkinstall -D--fstrans -pkgrelease 1-"$RELEASE" -pkgname php-fpm  -pkglicense PHP -pkggroup PHP -maintainer charlesportwoodii@ethreal.net -provides "php-fpm, php-fpm-5.6"  -requires "libxml2, libmcrypt4, libjpeg-turbo8" -replaces "php-fpm-5.5" -conflicts "php, php-common" -pakdir /tmp/ -y sh /tmp/php-$VERSION/setup
 
 mv /tmp/php-fpm_$VERSION-1_amd64.deb /tmp/php-fpm_$VERSION-1_amd64_$RELEASE.deb

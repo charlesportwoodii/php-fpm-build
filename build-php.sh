5385304c3ee867fd320d18e5a0e9b8e91a4f27e3
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-06-11 21:45:38 +0000

    Updating build, adding description.pak file

--- build-php.sh
+++ build-php.sh
@@ -3,9 +3,10 @@
 
 # Get the current script path
 SCRIPTPATH=`pwd -P`
-PCREVERSION=8.36
-OPENSSLVERSION=1.0.2a
+PCREVERSION=8.37
+OPENSSLVERSION=1.0.2b
 VERSION=$1
+CORES=$(grep -c ^processor /proc/cpuinfo)
 if [ -z "$2" ]
 then
     RELEASEVER=1;
@@ -39,15 +40,16 @@ cp setup /tmp/php-$VERSION/setup
 
 cd /tmp/php-$VERSION
 
-## Let PHP Build PCRE
-#wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$PCREVERSION.tar.gz
-#tar -xf pcre-$PCREVERSION.tar.gz
-#cd pcre-$PCREVERSION
-#./configure --enable-utf8
-#make
-#cd ..
+cd /tmp
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$PCREVERSION.tar.gz
+tar -xf pcre-$PCREVERSION.tar.gz
+cd pcre-$PCREVERSION
+./configure --enable-utf
+make -j$CORES
+cp pcre.h .libs/
 
 # Install the latest version of OpenSSL rather than using the libaries provided with the host OS
+#cd /tmp/php-$VERSION
 #wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 #tar -xf openssl-$OPENSSLVERSION.tar.gz
 
@@ -57,9 +59,12 @@ cd /tmp/php-$VERSION
 #cp sslconfig/patches/openssl__chacha20_poly1305_cf.patch .
 #patch -p1 < openssl__chacha20_poly1305_cf.patch
 
-#./config --prefix=/tmp/nginx\-$VERSION/openssl\-$OPENSSLVERSION/.openssl no-shared enable-ec_nistp_64_gcc_128 enable-tlsext
+#./config --prefix=/tmp/php\-$VERSION/openssl\-$OPENSSLVERSION/.openssl shared enable-ec_nistp_64_gcc_128 enable-tlsext
 #make depend
-#cd ..
+#make -j$CORES
+#make install
+#cd .openssl
+#ln -s lib lib64
 
 # Copy the Script Paths
 cd /tmp/php-$VERSION
@@ -74,7 +79,7 @@ cp $SCRIPTPATH/*-pak .
 	--with-gettext \
 	--with-iconv \
 	--with-openssl \
-	--with-pcre-regex \
+    --with-pcre-regex=/tmp/pcre-$PCREVERSION/.libs \
 	--with-zlib \
 	--with-layout=GNU \
 	--enable-exif \
@@ -107,14 +112,34 @@ cp $SCRIPTPATH/*-pak .
 	--enable-opcache \
 	--enable-soap \
 	--with-jpeg-dir \
-	--with-png-dir
+	--with-png-dir \
+	--enable-bcmath \
+	--disable-short-tags \
+	--enable-intl
+
+	# --with-openssl=/tmp/php-$VERSION/openssl-$OPENSSLVERSION/.openssl \
 
 # Install the init script
 $(which update-rc.d) /etc/init.d/php-fpm defaults
 
 mkdir -p /etc/php/conf.d
-make -j2
+make -j$CORES
 make install
 
 cd /tmp/php-$VERSION
-sudo checkinstall -D --fstrans -pkgrelease "$RELEASEVER"-"$RELEASE" -pkgrelease "$RELEASEVER"~"$RELEASE" -pkgname $RELEASENAME  -pkglicense PHP -pkggroup PHP -maintainer charlesportwoodii@ethreal.net -provides "php-fpm, php-fpm-"$major"."$mior  -requires "libxml2, libmcrypt4, libjpeg-turbo8" -replaces "php-fpm-5.5" -conflicts "php, php-common" -pakdir /tmp -y sh /tmp/php-$VERSION/setup
+checkinstall \
+	-D \
+	--fstrans \
+	-pkgrelease "$RELEASEVER"-"$RELEASE" \
+	-pkgrelease "$RELEASEVER"~"$RELEASE" \
+	-pkgname $RELEASENAME \
+	-pkglicense PHP \
+	-pkggroup PHP \
+	-maintainer charlesportwoodii@ethreal.net \
+	-provides "php-fpm, php-fpm-"$major"."$minor \
+	-requires "libxml2, libmcrypt4, libjpeg-turbo8" \
+	-replaces "php-fpm-5.5" \
+	-conflicts "php5, php5-common" \
+	-pakdir /tmp \
+	-y \
+	sh /tmp/php-$VERSION/setup
